7c668ccbcd8e6f33d4f538770f3d3273435e613f
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-06-06 08:43:08 -0600

    basic routing implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './session/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' },
+];
